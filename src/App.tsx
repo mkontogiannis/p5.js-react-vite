684684b1d0d1620c188194b85c5718d6bc064b8d
michael kontogiannis <mkontogiannis@protonmail.com>
2024-03-25 22:07:36 +0200

    feat: SEO component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { ReactP5Wrapper } from '@p5-wrapper/react';
 import { mySketch } from './sketches/mySketch';
+import { SEO } from './SEO';
 
 function App() {
-  return <ReactP5Wrapper sketch={mySketch} />;
+  return (
+    <>
+      <SEO />
+      <ReactP5Wrapper sketch={mySketch} />
+    </>
+  );
 }
 
 export default App;
